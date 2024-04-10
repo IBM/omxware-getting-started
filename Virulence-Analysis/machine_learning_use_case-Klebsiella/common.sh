65e2c668d62f76d2ee056b4d2c17caf2f3f41efb
Akshay.Agarwal1 <Akshay.Agarwal1@ibm.com>
2024-04-10 10:52:17 -0700

    added config and environment files

---
+++ Virulence-Analysis/machine_learning_use_case-Klebsiella/common.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+
+export SOURCE_PATH= #path for src folder
+export DATA_PATH= #path for data folder downloaded from PFDA
+export MODEL_PATH= #path for models downloaded from PFDA
+export RESULTS_PATH= #path for results folder downloaded from PFDA
+export DRY_RUN=False #change to True to perform a dry run
+export NUM_JOBS=50 #update on the basis of number of cores on machine
